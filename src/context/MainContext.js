e2294c2800ef722282288dd2e771802ef346b0d6
yusakman <yusakmanullang93@gmail.com>
2022-08-22 21:59:21 +0800

    Update data from App to MainContext

--- src/context/MainContext.js
+++ src/context/MainContext.js
@@ -1,5 +1,21 @@
-import { createContext } from "react";
+import React, { Component, createContext } from "react";
 
 export const MainContext = createContext({
-  empty: true,
+  key: true,
 });
+
+class MainContextProvider extends Component {
+
+  state = {
+    name: 'Lucy Chen',
+    ava: 'https://reqres.in/img/faces/7-image.jpg'
+  };
+
+  render() {
+    return (
+      <MainContext.Provider value={{...this.state}}>{this.props.children}</MainContext.Provider>
+    );
+  }
+}
+
+export default MainContextProvider
